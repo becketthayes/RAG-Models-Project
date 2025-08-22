4041a25f9c3630791f3adc6f1bc6a8f6f7c3c6f8
Beckett Hayes <becketthayes@gmail.com>
2025-08-21 23:03:29 -0700

    made initial changes to react frontend

---
+++ frontend/src/components/CreateModelForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CreateModelForm.css'; // Don't forget to import the CSS!
+
+function CreateModelForm() {
+    return (
+        <div className="form-card">
+            <h1>Create a New RAG Model</h1>
+            
+        </div>
+    )
+}
+
+export default CreateModelForm;
