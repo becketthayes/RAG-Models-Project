fc175de57110e124d86b723d61b728edaa853a7f
Beckett Hayes <becketthayes@gmail.com>
2025-08-25 11:19:00 -0700

    Added Auth0 authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,56 @@ import React, { useState } from 'react';
 import CreateModelForm from './components/CreateModelForm';
 import TopBar from './components/TopBar';
 import NavigationBar from './components/NavigationBar';
+import { useAuth0 } from "@auth0/auth0-react";
 
+function App() {
+  const [isCreating, setIsCreating] = useState(false);
+  const {
+    isLoading, // Loading state, the SDK needs to reach Auth0 on load
+    isAuthenticated,
+    error,
+    loginWithRedirect: login, // Starts the login flow
+    logout: auth0Logout, // Starts the logout flow
+    user, // User profile
+  } = useAuth0();
+
+  const signup = () =>
+    login({ authorizationParams: { screen_hint: "signup" } });
+
+  const logout = () =>
+    auth0Logout({ logoutParams: { returnTo: window.location.origin } });
+
+  if (isLoading) return "Loading...";
+
+  return isAuthenticated ? (
+    <>
+      <div className="page-layout">
+        <div className="main-content">
+          {isCreating ? (
+            <div className="form-container">
+              <CreateModelForm />
+            </div>
+          ) : (
+            <NavigationBar />
+          )}
+        </div>
+      </div>
+    </>
+  ) : (
+    <>
+      {error && <p>Error: {error.message}</p>}
+
+      <button onClick={signup}>Signup</button>
+
+      <button onClick={login}>Login</button>
+    </>
+  );
+}
+
+export default App;
+
+
+{/*
 function App() {
   const [isCreating, setIsCreating] = useState(false);
   return (
@@ -22,3 +71,4 @@ function App() {
 }
 
 export default App;
+*/}
